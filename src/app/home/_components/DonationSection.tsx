50b01b9a92cb7d58301eea6e2eae9753f29cfafe
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2025-04-03 12:55:07 +0100

    Chore: donation section setup

--- src/app/home/_components/DonationSection.tsx
+++ src/app/home/_components/DonationSection.tsx
@@ -15,19 +15,19 @@ function DonateSection() {
       </div>
 
         <div className='max-w-screen-2xl mx-auto '>
-            <div className="relative z-10 text-center max-w-3xl mx-auto p-6 inset-0 bg-white/80">
+            <div className="relative z-10 text-center max-w-7xl mx-auto p-6 inset-0 bg-white/80 rounded-2xl">
                 <h2 className="text-3xl md:text-4xl font-bold text-[#F9AE40] mb-6">DONATE HERE!</h2>
                 
-                <p className="text-lg md:text-xl text-white mb-6 leading-relaxed">
+                <p className="text-sm font-semibold text-[#2c7bbd] mb-6 leading-relaxed">
                 Your Financial Contributions to our Programs and services are pivotal to providing support and assistance
                 to people with neurodiversity.
                 </p>
                 
-                <p className="text-lg md:text-xl text-white mb-10 leading-relaxed">
+                <p className="text-sm font-semibold text-[#2c7bbd] mb-10 leading-relaxed">
                 Join our amazing donors and use your finances and other resources to make the world a better place.
                 </p>
 
-                <button className="bg-[#2c7bbd] hover:bg-[#1e5a9a] text-white font-bold py-4 px-10 rounded-full text-lg transition-colors duration-300">
+                <button className="bg-white  text-[#F9AE40] border border-[#F9AE40] font-bold py-4 px-10 rounded-full text-lg transition-colors duration-300">
                 MAKE A DONATION
                 </button>
             </div>
