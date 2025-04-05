287516fae438f2772e5ac926d16238a0405d6e0a
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2025-04-05 01:08:09 +0100

    Chore: Objectives setup

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ServiceHero from './_components/ServiceHero'
 import Objectives from './_components/Objectives'
+import Exploits from './_components/Exploits'
 
 function page() {
   return (
     <div>
         <ServiceHero/>
         <Objectives/>
+        <Exploits/>
     </div>
   )
 }
